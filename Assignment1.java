f4286f300b7c1092ba695bb0630cd7da36ce79a6
Saif-12 <47641013+Saif-12@users.noreply.github.com>
2024-02-12 06:50:46 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,20 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class Assignment1 {
+	public static void main(String[] args) {
+		System.out.println("Enter the number of rows and column");
+		Scanner s=new Scanner(System.in);
+		int row=s.nextInt();
+		int col=s.nextInt();
+		for(int i=0;i<row;i++)
+		{
+			for(int j=1;j<=col;j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
